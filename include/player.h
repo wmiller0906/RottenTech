9f1bef9331f8ab561b2eb907478200915ebd964b
wmiller0906 <wmiller0906@gmail.com>
2025-05-25 03:05:42 +0000

    Cleaned up Weapon class and created weapon_data.h

--- include/player.h
+++ include/player.h
@@ -1,44 +1,10 @@
-/*
-    This will include the class for the player as well as NPCs
-
-*/
-
 #ifndef PLAYER_H
 #define PLAYER_H
 
-using namespace std;
+#include <vector>   // For SPECIAL
 
 class Player {
-    public:
-        TakeDamage();
-    private:
-        int HP;
-        string name;
-}
-
-class NPC {
-    public:
     
-    protected:
-        int HP;
-        
-    
-}
-
-class HumanNPC {
-    public:
-    
-    protected:
-    
-}
-
-class AnimalNPC {
-    public:
-    
-    protected:
-    
-}
-
-
+};
 
 #endif
--- include/weapons.h
+++ include/weapons.h
@@ -2,6 +2,7 @@
 #define WEAPONS_H
 
 #include "item.h"   // Superclass library must be included in subclass header
+#include "weapon_data.h"
 #include <string>
 
 /*--------------------------------------------
@@ -9,22 +10,9 @@
 --------------------------------------------*/
 
 
-/*-----------------
+/*---------
 [11xxx] GUN
------------------*/
-
-// GunData : A configuration for defining types of guns
-struct GunData {
-    int id;
-    double weight;
-    int value;
-    std::string name;
-    double accuracy;
-    double damage;
-    double fireRate;
-    int capacity;
-    double reloadTime;
-};
+---------*/
 
 
 class Gun : public Item {
@@ -58,17 +46,6 @@ class Gun : public Item {
 [12xxx] MELEE
 ------------*/
 
-// meleeData : A configuration for defining types of melee weapons
-struct meleeData {
-    int id;
-    double weight;  // CHECK THESE VAR
-    int value;
-    std::string name;
-    double accuracy;
-    double damage;
-    double fireRate;
-};
-
 
 class Melee : public Item {
     public:
@@ -82,6 +59,10 @@ class Melee : public Item {
         const double damage;
         const double fireRate;
 };
+
+/*----------------
+[13xxx] THROWABLES
+----------------*/
 
 
 #endif
