83f0c64386318c42b03caa96c61d281e08507329
wmiller0906 <wmiller0906@gmail.com>
2025-05-25 03:02:55 +0000

    Moved gunData, meleeData, and throwData into weapon_data.h

---
+++ include/weapon_data.h
@@ -0,0 +1,43 @@
+#ifndef WEAPON_DATA_H
+#define WEAPON_DATA_H
+
+// GunData : A configuration for defining types of guns
+struct GunData {
+    int id;
+    double weight;
+    int value;
+    std::string name;
+    double accuracy;
+    double damage;
+    double fireRate;
+    int capacity;
+    double reloadTime;
+};
+
+
+// meleeData : A configuration for defining types of melee weapons
+struct meleeData {
+    int id;
+    double weight;
+    int value;
+    std::string name;
+    double accuracy;
+    double damage;
+    double fireRate;
+};
+
+
+// throwData : A configuration of defining types of throwable weapons
+struct throwData {
+    int id;
+    double weight;
+    int value;
+    std::string name;
+    double accuracy;
+    double damage;
+    double fireRate;
+    double stack;
+    double reloadTime;
+};
+
+#endif
