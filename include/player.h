28c5e9dd675859efa91ecbdde8d71842b66bb8d1
wmiller0906 <wmiller0906@gmail.com>
2025-05-27 00:19:12 +0000

    feat(player.h/.cpp): Began creating Player class

--- include/player.h
+++ include/player.h
@@ -1,10 +1,26 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-#include <vector>   // For SPECIAL
+#include "primary_stats.h"
+#include "npc.h"
 
 class Player {
-    
+    private:
+        double health;
+        double carryingCapacity;
+        int experience;
+        int level;
+        PrimaryStats primaryStats;
+        // secondary stats still needed
+    public:
+        void attack(NPC target);
+        // interact() - Needed? Text based so menu may handle this
+        void print_primary_stats() const;
+        //void print_secondary_stats() const;
+        int get_experience() const;
+        void add_experience(int xp);
+        int get_level() const;
+        // void level_up()
 };
 
 #endif
--- src/player.cpp
+++ src/player.cpp
@@ -1,1 +1,31 @@
 #include "player.h"
+
+void Player::attack(NPC target) {
+    /*
+      Need to select equipped weapon from inventory, .get_damage(), 
+      subtract that from NPC's health
+    */
+}
+
+void Player::print_primary_stats() const {
+    primaryStats.print_stats();
+}
+
+/*void Player::print_secondary_stats() const {
+    
+}*/
+
+int Player::get_experience() const { return experience; }
+void Player::add_experience(int xp) {
+    experience += xp;
+    // Add xp thresholds when level up is triggered. Then do something like follows:
+    /* 
+      nextLevel = levelUpGuide[get_level()];
+      if(get_experience() > nextLevel) { level_up(); }
+    */
+}
+int Player::get_level() const { return level; }
+
+/*void Player::level_up() {
+    // FINISH ME
+}*/
